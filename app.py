112c4948ad8c3606a0d5df1f5d48e1da266cebf6
fabitapia3108-code <fabitapia3108@gmail.com>
2026-06-14 20:54:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # Esta es tu página de inicio con bienvenida e intro
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/acerca-de')
+def acerca_de():
+    return render_template('acerca.html')
+
+@app.route('/oferta-educativa')
+def oferta():
+    return render_template('oferta.html')
+
+@app.route('/inscripcion')
+def inscripcion():
+    return render_template('inscripcion.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/aviso-privacidad')
+def aviso():
+    return render_template('aviso.html')
